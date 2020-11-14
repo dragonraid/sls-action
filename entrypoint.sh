7dcb708d6db00faffdef4081f5c726b2210d4f3b
dragonraid <lucass.novotny@gmail.com>
2020-11-14 12:46:47 +0100

    reverting and ignoring lint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+# shellcheck disable=SC2068
 # Using this script because github action passess the arguments as strings
 # instead of array
 echo ">>> Running sls $*"
-/app/node_modules/.bin/sls "$@"
+/app/node_modules/.bin/sls $@
 echo ">>> Completed"
