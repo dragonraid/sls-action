beb9ee153a09f0602d5632a2081fd153551d8e60
dragonraid <lucass.novotny@gmail.com>
2020-11-14 12:31:32 +0100

    fix path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 # Using this script because github action passess the arguments as strings
 # instead of array
 echo ">>> Running sls $@"
-"$(npm bin)"/sls $@
+/app/node_modules/.bin/sls $@
 echo ">>> Completed"
